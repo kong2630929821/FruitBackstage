acdaa24daedc521fb0db22ec3e0cdd35fc38193c
kong <2630929821@qq.com>
2019-04-28 18:24:05 +0800

    完成社区后台

--- model/communityModel.js
+++ model/communityModel.js
@@ -57,6 +57,160 @@ var communityModel={
         db.query(sql,(err,data)=>{
             fn(err,data);
         })
-    }
+    },
+    getAllComments:(id,fn)=>{
+        const sql='SELECT * FROM discuss WHERE p_id='+id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    getTheAllUser:(id,fn)=>{
+        id.forEach(v=>{
+            const sql='SELECT u_name,u_img FROM USER WHERE u_id='+v+';';
+            db.query(sql,(err,data)=>{
+                fn(err,data);
+            })
+        });
+    },
+    answer:(req,fn)=>{
+        const str=req.body;
+        const sql='INSERT INTO reply VALUES(NULL,'+str.p_id+','+str.r_meId+','+str.r_heId+',"'+str.r_time+'","'+str.r_content+'","'+str.r_meName+'","'+str.r_heName+'","'+str.r_meSrc+'",'+str.d_id+',NULL);';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        });
+    },
+    answered:(req,fn)=>{
+        const str=req.body;
+        const sql='INSERT INTO reply VALUES(NULL,'+str.p_id+','+str.r_meId+','+str.r_heId+',"'+str.r_time+'","'+str.r_content+'","'+str.r_meName+'","'+str.r_heName+'","'+str.r_meSrc+'",'+str.d_id+','+str.rr_id+');';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        });
+    },
+    getAllReplies:(p_id,fn)=>{
+        const sql='SELECT * FROM reply WHERE p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    changeReplyNum:(p_id,fn)=>{
+        const sql='UPDATE push SET p_discuss=p_discuss+1 WHERE p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        });
+    },
+    getDiscussNum:(p_id,fn)=>{
+        const sql='SELECT p_discuss FROM push WHERE p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    removeDiscuss:(p_id,d_id,fn)=>{
+        const sql='DELETE FROM discuss WHERE p_id='+p_id+'&& d_id='+d_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    removeReply:(r_id,d_id,p_id,fn)=>{
+        const sql='DELETE FROM reply WHERE d_id='+d_id+' && r_id='+r_id+' && p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    unChangeReplyNum:(p_id,n,fn)=>{
+        const sql='UPDATE push SET p_discuss=p_discuss-'+n+' WHERE p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        });
+    },
+    removeReplyed:(p_id,rr_id,r_id,fn)=>{
+        const sql='DELETE FROM reply WHERE p_id='+p_id+' && r_id='+r_id+' && rr_id='+rr_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        });
+    },
+    findUnNum:(p_id,r_id,fn)=>{
+        const sql='SELECT * FROM reply WHERE p_id='+p_id+' && rr_id='+r_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    findUnDisNum:(p_id,d_id,fn)=>{
+        const sql='SELECT * FROM reply WHERE p_id='+p_id+' && d_id='+d_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    like:(p_id,u_id,fn)=>{
+        const sql='INSERT INTO zan VALUES(NULL,'+u_id+','+p_id+');';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    findLikeNum:(p_id,fn)=>{
+        const sql='SELECT p_zan FROM push WHERE p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    setZan:(p_id,fn)=>{
+        const sql='UPDATE push SET p_zan=p_zan+1 WHERE p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    findLike:(p_id,u_id,fn)=>{
+        const sql='SELECT * FROM zan WHERE p_id='+p_id+' && u_id='+u_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    removeZan:(p_id,u_id,fn)=>{
+        const sql='DELETE FROM zan WHERE p_id='+p_id+' && u_id='+u_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    unSetZan:(p_id,fn)=>{
+        const sql='UPDATE push SET p_zan=p_zan-1 WHERE p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    collect:(p_id,u_id,fn)=>{
+        const sql='INSERT INTO collect VALUES(NULL,'+u_id+','+p_id+',NULL);';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    findCollectNum:(p_id,fn)=>{
+        const sql='SELECT p_collect FROM push WHERE p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    setCollect:(p_id,fn)=>{
+        const sql='UPDATE push SET p_collect=p_collect+1 WHERE p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    findCollect:(p_id,u_id,fn)=>{
+        const sql='SELECT * FROM collect WHERE p_id='+p_id+' && u_id='+u_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    removeCollect:(p_id,u_id,fn)=>{
+        const sql='DELETE FROM collect WHERE p_id='+p_id+' && u_id='+u_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
+    unSetCollect:(p_id,fn)=>{
+        const sql='UPDATE push SET p_collect=p_collect-1 WHERE p_id='+p_id+';';
+        db.query(sql,(err,data)=>{
+            fn(err,data);
+        })
+    },
 };
 module.exports=communityModel;
--- route/communityRoute.js
+++ route/communityRoute.js
@@ -6,4 +6,15 @@ myRouter.route('/release').post(communityController.release);//发布话题
 myRouter.route('/getAllTopics').post(communityController.getAllTopics);//获取全部话题
 myRouter.route('/getTheTopic').post(communityController.getTheTopic);//获取当前话题
 myRouter.route('/discuss').post(communityController.discuss);//发布评论
+myRouter.route('/getAllComments').post(communityController.getAllComments);//获取所有评论
+myRouter.route('/answer').post(communityController.answer);//回复评论人
+myRouter.route('/answered').post(communityController.answered);//回复回复人
+myRouter.route('/getAllReplies').post(communityController.getAllReplies);//获取所有回复
+myRouter.route('/removeDiscuss').post(communityController.removeDiscuss);//删除当前的评论
+myRouter.route('/removeReply').post(communityController.removeReply);//删除当前的回复
+myRouter.route('/removeReplyed').post(communityController.removeReplyed);//删除当前的回复的回复
+myRouter.route('/like').post(communityController.like);//点赞
+myRouter.route('/collect').post(communityController.collect);//点赞
+myRouter.route('/findLike').post(communityController.findLike);//获取当前用户是否点赞
+myRouter.route('/findCollect').post(communityController.findCollect);//获取当前用户是否收藏
 module.exports=myRouter;
